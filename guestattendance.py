84b39d4bef73241a5458ff37ee2c2f6558105be9
Claire <managerclaire@users.noreply.github.com>
2017-10-13 11:46:52 -0400

    bridal party
    added bridal party functionality

--- guestattendance.py
+++ guestattendance.py
@@ -2,8 +2,9 @@
 def totalguests():
     guests_in = int(input("In-town guests invited: "))
     guests_out = int(input("Out-of-town guests invited: "))
-    totalg = guests_in + guests_out
-    attendance = .65*guests_out + .9*guests_in
+    bridal_party = int(input("Bridal party: "))
+    totalg = guests_in + guests_out + bridal_party
+    attendance = .65*guests_out + .9*guests_in + bridal_party
     attendance = int(attendance)
     print("Approximately {} guests will attend.".format(attendance))
     pct_attend = (float(attendance)/float(totalg))*100
